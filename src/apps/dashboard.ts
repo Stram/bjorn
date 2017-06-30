773c4b4a46d1acb6d2fa259060af36f1de1b94a8
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-06-30 19:59:53 +0200

    Add simple dashboard application

---
+++ src/apps/dashboard.ts
@@ -0,0 +1,9 @@
+import * as Vue from 'vue';
+
+export default function createDashboardApp() {
+  const app = new Vue({
+    // TODO: Vue options
+  });
+
+  return {app};
+}
---
+++ src/client/dashboard/index.ts
@@ -0,0 +1,6 @@
+import createApp from '../../apps/dashboard';
+
+const {app} = createApp();
+
+// TODO: if element exists
+app.$mount('#app');
