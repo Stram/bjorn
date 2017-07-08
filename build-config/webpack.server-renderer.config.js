71ae240e09073cd348c68a9af64c3e25bb249c23
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-08 19:12:44 +0200

    Add Webpack config for SSR

---
+++ build-config/webpack.server-renderer.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const nodeExternals = require('webpack-node-externals')
+const VueSSRServerPlugin = require('vue-server-renderer/server-plugin')
+
+const baseConfig = require('./webpack.base.config.js');
+
+const srcFolder = path.resolve(__dirname, '../src');
+const distFolder = path.resolve(__dirname, '../dist/server-renderer-bundles');
+
+module.exports = merge(baseConfig, {
+  entry: {
+    dashboard: path.resolve(srcFolder, 'server/dashboard')
+  },
+
+  output: {
+    path: distFolder,
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+
+  devtool: 'source-map',
+
+  target: 'node',
+
+  // externals: nodeExternals({
+  //   // do not externalize dependencies that need to be processed by webpack.
+  //   // you can add more file types here e.g. raw *.vue files
+  //   // you should also whitelist deps that modifies `global` (e.g. polyfills)
+  //   whitelist: /\.css$/
+  // }),
+
+  plugins: [
+    new VueSSRServerPlugin()
+  ]
+});
