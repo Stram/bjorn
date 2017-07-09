98e1c58d63749276e15d1a3ef436fc1972705bce
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-09 13:21:25 +0200

    Add Abstract Model class

--- src/apps/dashboard/models/Widget.ts
+++ src/apps/dashboard/models/Widget.ts
@@ -1,14 +1,17 @@
+import Model from './Model';
+
 export interface widgetModelProps {
   width: number;
   height: number;
 }
 
-export default class Widget {
+export default class Widget extends Model {
 
   public width: number;
   public height: number;
 
   constructor({width, height}: widgetModelProps) {
+    super();
     this.width = width;
     this.height = height;
   }
