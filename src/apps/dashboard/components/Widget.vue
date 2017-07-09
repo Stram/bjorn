cee34cae4efb3667e831e44c673e822d951a9096
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-09 10:40:40 +0200

    Add Widget component

---
+++ src/apps/dashboard/components/Widget.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  import WidgetModel from 'apps/dashboard/models/Widget';
+
+  @Component({
+    props: {
+      widget: {
+        type: WidgetModel,
+        required: true,
+      }
+    }
+  })
+  export default class Widget extends Vue {
+
+  }
+</script>
