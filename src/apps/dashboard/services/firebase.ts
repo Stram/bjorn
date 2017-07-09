e25ed0f73811f11bad8117ff964f28f5ce292b2e
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-09 14:30:00 +0200

    Add template class for firebase service

---
+++ src/apps/dashboard/services/firebase.ts
@@ -0,0 +1,36 @@
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+export interface FirebaseConfig {
+  apiKey: string;
+}
+
+export default class Firebase {
+  private config: FirebaseConfig;
+  private app: firebase.app.App;
+  private database: firebase.database.Database;
+
+  constructor(config: FirebaseConfig) {
+    this.config = config;
+
+    this.initializeApplication();
+    this.initializeDatabase();
+  }
+
+  private initializeApplication() {
+    this.app = firebase.initializeApp(this.config);
+  }
+
+  private initializeDatabase() {
+    this.database = firebase.database();
+  }
+}
+
+
+// const firebaseConfig = {
+//   apiKey: "<API_KEY>",
+//   authDomain: "<PROJECT_ID>.firebaseapp.com",
+//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+//   storageBucket: "<BUCKET>.appspot.com",
+//   messagingSenderId: "<SENDER_ID>",
+// }
