9e586277488db4bd48903400e8c4fba27d224a54
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-14 15:28:15 +0200

    Move firebase config object to config file

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,1 +1,8 @@
 export const APP_ROOT_ID = 'app';
+export const firebaseConfig = {
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+};
--- src/app/services/firebase.ts
+++ src/app/services/firebase.ts
@@ -25,12 +25,3 @@ export default class Firebase {
     this.database = firebase.database();
   }
 }
-
-
-// const firebaseConfig = {
-//   apiKey: "<API_KEY>",
-//   authDomain: "<PROJECT_ID>.firebaseapp.com",
-//   databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-//   storageBucket: "<BUCKET>.appspot.com",
-//   messagingSenderId: "<SENDER_ID>",
-// }
