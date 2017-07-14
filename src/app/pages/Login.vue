87a0a0fc43c28aa35cbb8ecfcf0942fac6958558
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-14 18:46:54 +0200

    Add Login page component

---
+++ src/app/pages/Login.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <div>
+    It seems like you are not authorized.
+
+    <LoginForm 
+      :form="loginForm"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  import LoginFormComponent from 'app/components/forms/LoginForm.vue';
+  import LoginForm from 'app/forms/LoginForm';
+
+  @Component({
+    components: {
+      LoginForm: LoginFormComponent
+    }
+  })
+  export default class Grid extends Vue {
+    loginForm = new LoginForm({store: this.$store});
+  }
+</script>
+
+<style lang="scss" module>
+
+</style>
