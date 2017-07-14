403582a6158422432b82cbb46dd6e4258650a23c
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-14 15:43:30 +0200

    Update config for firebase connection

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,10 @@
 export const APP_ROOT_ID = 'app';
+
 export const firebaseConfig = {
-  apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  storageBucket: "<BUCKET>.appspot.com",
-  messagingSenderId: "<SENDER_ID>",
+  apiKey: 'AIzaSyCtBrFy67it5vDeUQDpMG8N0iBAZhfUMc4',
+  authDomain: 'dashboard-9f23b.firebaseapp.com',
+  databaseURL: 'https://dashboard-9f23b.firebaseio.com',
+  projectId: 'dashboard-9f23b',
+  storageBucket: 'dashboard-9f23b.appspot.com',
+  messagingSenderId: '674484305664'
 };
