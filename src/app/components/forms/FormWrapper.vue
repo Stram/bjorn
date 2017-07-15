8122b21cbe2f148efa4da6b558055215aadf2d56
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-15 20:34:38 +0200

    Add FormWrapper component

---
+++ src/app/components/forms/FormWrapper.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <form
+    @submit.prevent="onSubmit"
+  >
+    <slot></slot>
+  </form>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  import {Prop} from 'vue-property-decorator';
+
+  import Form from 'app/forms/Form';
+
+  @Component({})
+  export default class FormWrapper extends Vue {
+    @Prop({type: Object, required: true})
+    private form: Form;
+
+    onSubmit() {
+      console.log(this.form);
+
+      this.form.submit();
+    }
+  }
+</script>
+
+<style lang="scss" module>
+
+</style>
