d74622062a79e13c72ac6f346d12d1a1a6c71f14
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-15 20:36:50 +0200

    Update mutations for session module in Vuex store

--- src/app/store/modules/session/mutations.ts
+++ src/app/store/modules/session/mutations.ts
@@ -1,10 +1,12 @@
 import {MutationTree} from 'vuex';
 
 import * as mutationTypes from 'app/store/mutation-types';
+import User from 'app/models/User';
+
 import State from './state';
 
 export default <MutationTree<State>> {
-  [mutationTypes.CHANGE_USER](state: State) {
-    state.user = '';
+  [mutationTypes.SET_USER](state: State, user: User) {
+    state.user = user;
   }
 }
