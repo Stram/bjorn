34c9853536ba77bdb4d9fc785b5c2608cd6b35b6
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-15 20:36:35 +0200

    Update getters for session module in Vuex store

--- src/app/store/modules/session/getters.ts
+++ src/app/store/modules/session/getters.ts
@@ -1,11 +1,18 @@
 import {GetterTree} from 'vuex';
 
+import User from 'app/models/User';
+
 import State from './state';
 
-export function currentUser(state: State): number {
-  return 0;
+export function currentUser(state: State): User {
+  return state.user;
+}
+
+export function isAuthenticated(state: State): boolean {
+  return state.isLoaded && !state.isLoading && Boolean(state.user);
 }
 
 export default <GetterTree<State, any>> {
   currentUser,
+  isAuthenticated
 }
