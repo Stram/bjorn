666e9e2908854ff5e5058bc1e1ad2e6d7d24466a
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-29 15:50:20 +0200

    Add simple action layout

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,8 @@
+// FORMS
 export { default as LoginForm } from './forms/LoginForm.vue';
+
+// WIDGETS
 export { default as TimeWidget } from './widgets/Time.vue';
+
+// LAYOUTS
+export { default as SimpleActionLayout } from './layouts/SimpleAction.vue';
---
+++ src/app/components/layouts/SimpleAction.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="$style.container">
+    <slot name="header" />
+    <slot name="footer" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({})
+export default class SimpleActionLayout extends Vue {
+  
+}
+</script>
+
+<style lang="scss" module>
+
+  .container {
+    background-color: var(--color-primary);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    min-height: 100vh;
+    padding: 80px;
+  }
+</style>
