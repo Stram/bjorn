9fac95eb31e59206e26aaee8278d67444aac1a1b
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-07-29 15:42:07 +0200

    Add flags for VUE_ENV in config file

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,12 @@
 export const APP_ROOT_ID = 'app';
+export const isServer = process.env.VUE_ENV === 'server';
+export const isClient = process.env.VUE_ENV === 'client';
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyCtBrFy67it5vDeUQDpMG8N0iBAZhfUMc4',
   authDomain: 'dashboard-9f23b.firebaseapp.com',
   databaseURL: 'https://dashboard-9f23b.firebaseio.com',
+  messagingSenderId: '674484305664',
   projectId: 'dashboard-9f23b',
   storageBucket: 'dashboard-9f23b.appspot.com',
-  messagingSenderId: '674484305664'
 };
