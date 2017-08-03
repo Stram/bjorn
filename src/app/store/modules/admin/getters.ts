9e2db0edf328d4ad773ab5353dbcc736f1f1a98a
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-03 22:18:57 +0200

    Add admin store module getters

---
+++ src/app/store/modules/admin/getters.ts
@@ -0,0 +1,11 @@
+import Dashboard from 'app/models/Dashboard';
+
+import State from './state';
+
+export function dashboards(state: State): Array<Dashboard> {
+  return state.dashboards;
+}
+
+export default {
+  dashboards,
+};
