4eb8cd22071524204df56a96f679f458d14cf7d2
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-05 20:41:58 +0200

    Add widget type enumeration

---
+++ src/app/enums/widgets.ts
@@ -0,0 +1,9 @@
+import { TimeWidget } from 'app/components';
+import TimeModel from 'app/models/widgets/Time';
+
+export default {
+  TIME_WIDGET: {
+    component: TimeWidget,
+    model: TimeModel,
+  },
+};
