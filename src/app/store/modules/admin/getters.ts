4ce8263c9d7de7f2de29e51eca251b1216cc40af
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-05 10:15:06 +0200

    Add sorting of dashboards

--- src/app/store/modules/admin/getters.ts
+++ src/app/store/modules/admin/getters.ts
@@ -3,7 +3,11 @@ import Dashboard from 'app/models/Dashboard';
 import State from './state';
 
 export function dashboards(state: State): Array<Dashboard> {
-  return state.dashboards.data;
+  return state.dashboards.data.sort((dashboardA, dashboardB) => {
+    const createdAtA = new Date(dashboardA.createdAt);
+    const createdAtB = new Date(dashboardA.createdAt);
+    return createdAtA.getMilliseconds() - createdAtB.getMilliseconds();
+  });
 }
 
 export default {
