8ff4dbc8cd53378a69c9a6f5518eb4978c83eb04
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-06 11:09:37 +0200

    Add design for empty widget

--- src/app/components/admin/DashboardPreview.vue
+++ src/app/components/admin/DashboardPreview.vue
@@ -3,17 +3,20 @@
     :class="$style.container"
     :style="gridStyle"
   >
-    <div
+    <button
       v-for="widget in widgetsStructure"
       :key="widget.uid"
       :style="{
         gridColumn: `${widget.x + 1} / span ${widget.width}`,
         gridRow: `${widget.y + 1} / span ${widget.height}`
       }"
+      :class="[$style.widget, {
+        [$style.empty]: widget.type === 'empty'
+      }]"
+      @click="onWidgetClick(widget)"
     >
-      Ono je prazan widget
-    </div>
-
+      <inline-svg :src="plusIcon" :class="$style.icon" />
+    </button>
   </section>
 </template>
 
@@ -22,11 +25,21 @@
   import { Component, Prop } from 'vue-property-decorator';
   import { times } from 'lodash';
 
+  import { InlineSVG } from 'app/components';
+
   import Dashboard from 'app/models/Dashboard';
   import Widget from 'app/models/Widget';
 
-  @Component({})
+  import * as plusIcon from 'images/plus.svg';
+
+  @Component({
+    components: {
+      'inline-svg': InlineSVG
+    }
+  })
   export default class DashboardPreview extends Vue {
+    plusIcon = plusIcon
+
     @Prop({type: Object, required: true})
     private dashboard: Dashboard;
 
@@ -58,19 +71,57 @@
       return widgets;
     }
 
-    onCreateClick() {
-      this.$emit('dashboard:create');
+    onWidgetClick(widget: Widget) {
+      if (widget.type === 'empty') {
+
+      }
     }
   }
 </script>
 
 <style lang="scss" module>
+  $transition-speed: 150ms;
+
   .container {
     display: grid;
-    grid-gap: 10px;
+    grid-gap: 24px;
     align-items: stretch;
     justify-content: stretch;
     margin: 48px 64px;
+    padding: 20px;
     border: 5px solid var(--color-secondary);
+  }
+
+  .widget {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 5px solid var(--color-secondary);
+    transition: border-color $transition-speed;
+
+    &.empty {
+      border-style: dashed;
+    }
+
+    &:hover {
+      border-color: var(--color-primary);
+
+      .icon {
+        svg {
+          path {
+            fill: var(--color-primary);
+          }
+        }
+      }
+    }
+  }
+
+  .icon {
+    svg {
+      path {
+        fill: var(--color-secondary);
+        transition: fill $transition-speed;
+      }
+    }
   }
 </style>
