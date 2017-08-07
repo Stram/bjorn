3ebd5751f4786cc2096f0ccb7c1b3f9c7e72262b
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-07 09:51:53 +0200

    Add spinner component

---
+++ src/app/components/utils/Spinner.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div :class="$style.wrapper">
+    <div :class="$style.content"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({})
+export default class Spinner extends Vue {
+
+}
+</script>
+
+<style lang="scss" module>
+$spinner-size: 50px;
+
+@keyframes spinner-rotating-wrapper {
+  0% {
+    transform: rotate(0deg)
+  }
+
+  100% {
+    transform: rotate(180deg)
+  }
+}
+
+.wrapper {
+  width: $spinner-size;
+  height: $spinner-size;
+  border: 5px solid var(--color-light);
+  position: relative;
+  animation-name: spinner-rotating-wrapper;
+  animation-delay: 0s;
+  animation-duration: 1s;
+}
+
+.content {
+  height: 0%;
+  width: 100%;
+}
+</style>
