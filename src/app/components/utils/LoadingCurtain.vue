756e278b64cd525efebe91c529c929692b323817
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-07 09:52:07 +0200

    Add loading curtain component

---
+++ src/app/components/utils/LoadingCurtain.vue
@@ -0,0 +1,35 @@
+<template lang="html">
+  <div :class="$style.curtain">
+     <app-spinner /> 
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
+
+  import AppSpinner from 'app/components/utils/Spinner.vue';
+
+  @Component({
+    components: {
+      AppSpinner,
+    }
+  })
+  export default class LoadingCurtain extends Vue {
+
+  }
+</script>
+
+<style lang="scss" module>
+
+.curtain {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
