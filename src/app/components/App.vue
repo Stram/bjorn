4f6bd1df02910ff82b5e47f77e8f02f91c3b2850
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-07 09:52:25 +0200

    Add loading curtain to App root component

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,19 +1,31 @@
 <template lang="html">
   <main :id="config.APP_ROOT_ID">
-    <router-view></router-view>
+    <router-view></router-view>  
+    <loading-curtain
+      v-if="showLoadingCurtain"
+    />     
   </main>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import Component from 'vue-class-component'
+  import { Component } from 'vue-property-decorator';
 
-  import {APP_ROOT_ID} from '../config';
+  import { APP_ROOT_ID } from 'app/config';
+  import LoadingCurtain from 'app/components/utils/LoadingCurtain.vue';
 
-  @Component({})
+  @Component({
+    components: {
+      LoadingCurtain,
+    }
+  })
   export default class App extends Vue {
     config = {
       APP_ROOT_ID
+    }
+
+    get showLoadingCurtain() {
+      return this.$store.getters['isLoading'];
     }
   }
 </script>
