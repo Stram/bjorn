119aed9886ac97cb252e465c189c4d3bac8e82ce
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-11 19:29:27 +0200

    Add component for new widget page

---
+++ src/app/pages/admin/widget/New.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div>asudbaifbgifubgiaufglfibgilfdg</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component({
+  components: {
+
+  }
+})
+export default class AdminWidgetNewPage extends Vue {
+
+}
+</script>
+
+<style lang="scss" module>
+
+</style>
