78a4e2c6acaf489f067880656da023bb421f846e
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-12 23:20:20 +0200

    Add close action to new widget page component

--- src/pages/admin/widget/New.vue
+++ src/pages/admin/widget/New.vue
@@ -1,6 +1,7 @@
 <template>
   <app-modal
     title="Select widget type"
+    @close="onModalClose"
   >
     <div>List of widget types</div>
   </app-modal>
@@ -9,9 +10,19 @@
 <script>
 import AppModal from 'components/utils/Modal.vue';
 
+import { pages } from 'router';
+
 export default {
   components: {
     AppModal,
+  },
+
+  methods: {
+    onModalClose() {
+      this.$router.push({
+        name: pages.ADMIN_DASHBOARD_INDEX
+      })
+    }
   }
 }
 </script>
