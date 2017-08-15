e3fa4584b5af6c27c7055a1c6c242e207cc28ac9
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-15 17:04:36 +0200

    Add action footer component

---
+++ src/components/admin/ActionFooter.vue
@@ -0,0 +1,143 @@
+<template>
+  <footer :class="$style.footer">
+    <section :class="[$style.section, $style.dashboard]">
+      <div v-if="dashboard">
+        <h1 :class="$style.title">Dashboard</h1>
+        <div :class="$style.name">{{dashboard.id}}</div>
+      </div>
+      <div v-if="dashboard">
+        ACTIONS
+      </div>
+    </section>
+    <section :class="[$style.section, $style.widget]">
+      <template v-if="widget">
+        <div>
+          <h1 :class="$style.title">Widget</h1>
+          <div :class="$style.type">{{widget.type}}</div>
+        </div>
+        <div :class="$style.widgetActions">
+          <app-button
+            :theme="buttonThemes.OUTLINE"
+            @click="onWidgetChangeOptionsClick"
+          >
+            Change options
+          </app-button>
+
+          <app-button
+            :theme="buttonThemes.OUTLINE"
+            @click="onWidgetChangeDataClick"
+          >
+            Change data
+          </app-button>
+
+          <app-button
+            :theme="buttonThemes.OUTLINE"
+            @click="onWidgetRemoveClick"
+          >
+            Remove
+          </app-button>
+        </div>
+        <div>
+          <!-- TODO -->
+        </div>
+        <div>
+          <!-- TODO -->
+        </div>
+      </template>
+    </section>
+  </footer>
+</template>
+
+<script>
+  import AppButton from 'components/utils/Button';
+
+  import buttonThemes from 'enums/button-themes';
+
+  export default {
+    components: {
+      AppButton
+    },
+
+    props: {
+      dashboard: {},
+      widget: {},
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+      onWidgetChangeOptionsClick() {
+
+      },
+
+      onWidgetChangeDataClick() {
+
+      },
+
+      onWidgetRemoveClick() {
+
+      }
+    },
+
+    data() {
+      return {
+        buttonThemes
+      };
+    }
+  };
+</script>
+
+<style lang="scss" module>
+  .footer {
+    display: flex;
+  }
+
+  .section {
+    padding: 24px;
+    display: flex;
+  }
+
+  .dashboard {
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: var(--color-primary);
+    min-width: 300px;
+
+    .title {
+      color: var(--color-secondary);
+    }
+  }
+
+  .widget {
+    flex: 1;
+    background-color: var(--color-secondary);
+
+    .title {
+      color: var(--color-light);
+    }
+  }
+
+  .title {
+    font-size: 36px;
+    font-weight: bold;
+  }
+
+  .name {
+    font-size: 36px;
+    color: var(--color-light);
+  }
+
+  .type {
+    font-size: 24px;
+    color: var(--color-light);
+  }
+
+  .widget-actions {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+</style>
