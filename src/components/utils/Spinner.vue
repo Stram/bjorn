458d3478e5b01f049f2088a5cae7151a9af6b2d2
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-18 23:05:09 +0200

    Refactor Spinner component

--- src/components/utils/Spinner.vue
+++ src/components/utils/Spinner.vue
@@ -4,75 +4,72 @@
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+<script>
 
-@Component({})
-export default class Spinner extends Vue {
+  export default {
 
-}
+  };
 </script>
 
 <style lang="scss" module>
-$spinner-size: 50px;
-$animation-time: 3s;
+  $spinner-size: 50px;
+  $animation-time: 3s;
 
-@keyframes spinner-rotating-wrapper {
-  0% {
-    transform: rotate(0deg)
+  @keyframes spinner-rotating-wrapper {
+    0% {
+      transform: rotate(0deg)
+    }
+
+    25% {
+      transform: rotate(180deg)
+    }
+
+    50% {
+      transform: rotate(180deg)
+    }
+
+    75% {
+      transform: rotate(0deg)
+    }
   }
 
-  25% {
-    transform: rotate(180deg)
+  @keyframes spinner-fill {
+    0% {
+      transform: translateY(0%)
+    }
+
+    25% {
+      transform: translateY(0%)
+    }
+
+    50% {
+      transform: translateY(-100%)
+    }
+
+    75% {
+      transform: translateY(-100%)
+    }
   }
 
-  50% {
-    transform: rotate(180deg)
+  .wrapper {
+    width: $spinner-size;
+    height: $spinner-size;
+    border: 5px solid var(--color-light);
+    position: relative;
+    animation-name: spinner-rotating-wrapper;
+    overflow: hidden;
   }
 
-  75% {
-    transform: rotate(0deg)
-  }
-}
-
-@keyframes spinner-fill {
-  0% {
-    transform: translateY(0%)
+  .content {
+    height: 100%;
+    width: 100%;
+    background-color: var(--color-primary);
+    animation-name: spinner-fill;
   }
 
-  25% {
-    transform: translateY(0%)
+  .wrapper,
+  .content {
+    animation-duration: $animation-time;
+    animation-iteration-count: infinite;
   }
-
-  50% {
-    transform: translateY(-100%)
-  }
-
-  75% {
-    transform: translateY(-100%)
-  }
-}
-
-.wrapper {
-  width: $spinner-size;
-  height: $spinner-size;
-  border: 5px solid var(--color-light);
-  position: relative;
-  animation-name: spinner-rotating-wrapper;
-  overflow: hidden;
-}
-
-.content {
-  height: 100%;
-  width: 100%;
-  background-color: var(--color-primary);
-  animation-name: spinner-fill;
-}
-
-.wrapper,
-.content {
-  animation-duration: $animation-time;
-  animation-iteration-count: infinite;
-}
 </style>
