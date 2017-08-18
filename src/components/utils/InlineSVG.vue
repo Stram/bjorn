1f91168d1f8487bf6040cc63b44bb9218e19b9ee
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-18 23:04:53 +0200

    Refactor InlineSVG component

--- src/components/utils/InlineSVG.vue
+++ src/components/utils/InlineSVG.vue
@@ -2,23 +2,13 @@
   <div v-html="src"></div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
-
-@Component({
-  props: {
-    src: {
-      type: String,
-      required: true,
+<script>
+  export default {
+    props: {
+      src: {
+        type: String,
+        required: true,
+      }
     }
-  }
-})
-export default class InlineSVG extends Vue {
-
-}
+  };
 </script>
-
-<style lang="scss" module>
-
-</style>
