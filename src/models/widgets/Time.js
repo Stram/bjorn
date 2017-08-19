8ec282bb0ef55c11575f64c7405302dc209b8ef7
Stjepan Petrusa <stjepan.petrusa@infinum.hr>
2017-08-19 20:38:44 +0200

    Remove time widget model

--- src/models/widgets/Time.js
+++
@@ -1,8 +0,0 @@
-import Widget from 'models/Widget';
-
-export default class TimeWidget extends Widget {
-  constructor(props) {
-    super(props);
-    this.refreshInterval = props.refreshInterval;
-  }
-}
